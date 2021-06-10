dc21cdd49d9e712560a16633d1cb5c1a1e569855
luna215 <paulluna0215@gmail.com>
2021-06-10 11:22:57 -0700

    adding dummy env variable

--- app.py
+++ app.py
@@ -7,3 +7,4 @@ import os
 for i in range(10000):
     print('second pt6', i)
     print('What is the variables:', os.environ['ENVIRONMENT'])
+    print('nothing else huh', os.environ.get('ANOTHER_VARIABLE'))
